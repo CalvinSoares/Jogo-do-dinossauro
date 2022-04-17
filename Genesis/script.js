94a5d0582a009dfa89a925464066f338ce279300
Calvin <calvinsoares@17gmail.com>
2022-04-16 21:07:44 -0300

    código para o dinossauro pular completo

---
+++ Genesis/script.js
@@ -0,0 +1,37 @@
+const dino = document.querySelector('.dino');
+let isjumping = false;
+
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isjumping) {
+         jump();
+        }
+    }
+}
+
+function jump() {
+    let position = 0;
+    isjumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isjumping = false;
+                } else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
