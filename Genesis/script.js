5efb23bf1d73decff7aae1a85982936913433a2d
Calvin <calvinsoares@17gmail.com>
2022-04-17 12:00:30 -0300

    pulo e ponto de contato adicionados

--- Genesis/script.js
+++ Genesis/script.js
@@ -1,7 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isjumping = false;
-
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -12,7 +12,7 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
+    
     isjumping = true;
 
     let upInterval = setInterval(() => {
@@ -38,15 +38,29 @@ function jump() {
 function creatCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
 
     let leftInterval = setInterval(() => {
-        cactusPosition -= 18;
-        cactus.style.left = cactusPosition + 'px';
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+        
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+       } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
     }, 20);
+
+    setTimeout(creatCactus, randomTime);
 }
 
 creatCactus();
